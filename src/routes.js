d963e799517607077d098a3b7d93f3dba2b9204e
Renan Oliveira <renanoliveira2199@gmail.com>
2019-06-11 04:00:26 -0300

    404

--- src/routes.js
+++ src/routes.js
@@ -1,27 +1,28 @@
 import React from "react";
 
-import { HashRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Main from "./pages/main";
 import Product from "./pages/product";
 import Error from "./pages/error";
 
 const Routes = () => (
-    <HashRouter>
-        {/* <Switch> */}
+    <BrowserRouter>
+        <Switch>
             {/* Main */}
-            <Route exact path="/" component={Main} />
-            <Route exact path="/home" component={Main} />
-            <Route exact path="/app-react" component={Main} />
+            {/* <Route exact path="/" component={Main} /> */}
+            {/* <Route exact path="/home" component={Main} /> */}
+            <Route exact path="/app-react/" component={Main} />
+            <Route exact path="/app-react/products" component={Main} />
 
             {/* Product */}
-            <Route exact path="/app-react/products" component={Product} />
             <Route exact path="/app-react/products/:id" component={Product} />
 
             {/* Error */}
             <Route component={Error} />
-        {/* </Switch> */}
-    </HashRouter>
+            <Route path="/app-react/erro" component={Error} />
+        </Switch>
+    </BrowserRouter>
 );
 
 export default Routes;
