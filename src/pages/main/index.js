69fa05e0633aa55e59a1a68ab737c3426ae1280d
Renan Oliveira <renanoliveira2199@gmail.com>
2019-06-13 22:58:01 -0300

    opacity

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import api from "../../services/api";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import "./styles.css";
 
@@ -8,18 +8,28 @@ export default class Main extends Component {
     state = {
         products: [],
         productInfo: {},
-        page: 1
+        page: 1,
+        opacity: 1
     };
+
+    componentWillMount() {
+        this.loadingProducts();
+    }
+
     componentDidMount() {
         this.loadProducts();
     }
+
+    loadingProducts = async () => {
+        this.setState({ opacity: 0 });
+    };
 
     loadProducts = async (page = 1) => {
         const response = await api.get(`/products?page=${page}`);
 
         const { docs, ...productInfo } = response.data;
 
-        this.setState({ products: docs, productInfo, page });
+        this.setState({ products: docs, productInfo, page, opacity: 1 });
     };
 
     prevPage = () => {
@@ -43,20 +53,27 @@ export default class Main extends Component {
     };
 
     render() {
-        const { products,page,productInfo } = this.state;
+        const { products, page, productInfo, opacity } = this.state;
 
         return (
-            <div className="product-list">
+            <div className="product-list" style={{opacity: opacity}}>
                 {products.map(product => (
                     <article key={product._id}>
                         <strong>{product.title}</strong>
                         <p>{product.description}</p>
                         <Link to={`/products/${product._id}`}>Acessar</Link>
                     </article>
-                ))} 
+                ))}
                 <div className="actions">
-                    <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
-                    <button disabled={page === productInfo.page} onClick={this.nextPage}>Próxima</button>
+                    <button disabled={page === 1} onClick={this.prevPage}>
+                        Anterior
+                    </button>
+                    <button
+                        disabled={page === productInfo.page}
+                        onClick={this.nextPage}
+                    >
+                        Próxima
+                    </button>
                 </div>
             </div>
         );
